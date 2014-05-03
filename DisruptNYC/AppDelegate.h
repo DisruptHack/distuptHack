a17b16b34fae51b33269b9c7443ba9ee9f4ac3d4
Emin <Israfil@Emins-MacBook-Pro.local>
2014-05-03 16:11:35 -0400

    Initial Commit

---
+++ DisruptNYC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DisruptNYC
+//
+//  Created by Emin on 5/3/14.
+//  Copyright (c) 2014 Disrupt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
