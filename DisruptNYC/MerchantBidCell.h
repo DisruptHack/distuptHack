7aefe27e39d8ede1ea5ea6ad9872c32034c1ce46
Emin <Israfil@Emins-MacBook-Pro.local>
2014-05-03 16:23:44 -0400

    MerchantBidCell

---
+++ DisruptNYC/MerchantBidCell.h
@@ -0,0 +1,13 @@
+//
+//  MerchantBidCell.h
+//  DisruptNYC
+//
+//  Created by Emin on 5/3/14.
+//  Copyright (c) 2014 Disrupt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MerchantBidCell : UITableViewCell
+
+@end
---
+++ DisruptNYC/MerchantBidCell.m
@@ -0,0 +1,34 @@
+//
+//  MerchantBidCell.m
+//  DisruptNYC
+//
+//  Created by Emin on 5/3/14.
+//  Copyright (c) 2014 Disrupt. All rights reserved.
+//
+
+#import "MerchantBidCell.h"
+
+@implementation MerchantBidCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
