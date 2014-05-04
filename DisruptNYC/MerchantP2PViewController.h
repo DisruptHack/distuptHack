14a615b4899bfe91a9157efe8c08e86229f5ebef
Rene Saroza <rsaroza@rasgongroup.com>
2014-05-04 04:06:22 -0400

    Added P2P changes

---
+++ DisruptNYC/MerchantP2PViewController.h
@@ -0,0 +1,14 @@
+//
+//  MerchantViewController.h
+//  P2PMerchant
+//
+//  Created by local admin on 5/3/14.
+//  Copyright (c) 2014 local admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@import MultipeerConnectivity;
+
+@interface MerchantP2PViewController : UIViewController <MCSessionDelegate, MCBrowserViewControllerDelegate>
+
+@end
